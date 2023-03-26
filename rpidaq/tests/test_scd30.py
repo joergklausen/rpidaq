1a01cd426b5a9519dde7895eaee82502e5d7b034
Jörg Klausen <joerg.klausen@meteoswiss.ch>
2023-03-26 13:38:02 +0200

    v0.1.1 test_scd30

---
+++ rpidaq/tests/test_scd30.py
@@ -0,0 +1,48 @@
+"""
+Credits: https://github.com/dvsu/sps30
+
+MIT License
+
+Copyright (c) 2021 Dave
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import sys
+import json
+from time import sleep
+from scd30.scd30 import SCD30
+
+
+if __name__ == "__main__":
+    c02_sensor = SPS30()
+    print(f"Serial number: {c02_sensor.get_serial_number()}")
+    print(f"Status register: {c02_sensor.get_status_register()}")
+    c02_sensor.start_measurement()
+    sleep(5)
+    
+    while True:
+        try:
+            print(json.dumps(c02_sensor.get_measurement(), indent=2))
+            sleep(2)
+
+        except KeyboardInterrupt:
+            print("Stopping measurement...")
+            c02_sensor.stop_measurement()
+            sys.exit()
